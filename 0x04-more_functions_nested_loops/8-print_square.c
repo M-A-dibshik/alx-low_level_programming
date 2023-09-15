a4c3f91750e24add9bcb7b39048988e3ce628907
Mohammed Abdalla <dibshik55@gmail.com>
2023-09-14 19:06:18 -0700

    t

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,13 +11,11 @@
 void print_square(int size)
 {
 	int row, column;
-	if(size > 0)
-
 
 	for (row = 1; row <= size; row++)
 	{
 		for (column = 1; column <= size; column++)
-			_putchar('#');
-		_putchar('\n');
+				_putchar('#');
+			_putchar('\n');
 	}
 }
