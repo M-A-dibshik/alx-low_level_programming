aa5b0f4ae4f347d90f90da10bbc8198ecc219c2c
Mohammed Abdalla <dibshik55@gmail.com>
2023-09-25 13:13:08 -0700

    f5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,14 +13,23 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 
+	if (*needle == 0)
+		return (haystack);
+
 	while (*haystack)
 	{
-		for (i = 0; needle[i]; i++)
+		i = 0;
+
+		if (haystack[i] == needle[i])
 		{
-		if (*haystack == needle[i])
-		return (haystack);
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
 		}
-	haystack++;
+
+		haystack++;
 	}
 	return ('\0');
 }
