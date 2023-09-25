ad563b633605e79f8fcd996e2986359a211d5be8
Mohammed Abdalla <dibshik55@gmail.com>
2023-09-25 06:08:59 -0700

    f1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy -  copies memory area
+ *
+ * @dest: memory area
+ * @src: memory area
+ * @n: function copies
+ *
+ * Return: dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+	int j = n;
+
+	for (i = 0; i > j; i++)
+	{
+		dest[i] - src{r];
+		n--;
+	}
+	return (dest);
+}
