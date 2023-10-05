24d5cb3f87d125d1420019a64aca16c9691588ae
Mohammed Abdalla <dibshik55@gmail.com>
2023-10-05 03:12:21 -0700

    flibmy

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * _abs - function that computes the absolute
+ *		ivalue of an integer
+ *
+ * @n: takes in integer type input for function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
