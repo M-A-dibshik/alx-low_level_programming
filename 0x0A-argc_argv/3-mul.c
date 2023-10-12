9ace4c046aeb008191893e44fe344fdd50f0873e
Mohammed Abdalla <dibshik55@gmail.com>
2023-10-12 16:08:13 -0700

    f3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that multiplies two numbers
+ * @argc: number of command line arguments
+ * @argv: pointer to an array of command line argumens
+ * Return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
