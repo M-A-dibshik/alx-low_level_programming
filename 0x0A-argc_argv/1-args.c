cca91684d1419f2398ac0c74aad0eb3f9da2fa96
Mohammed Abdalla <dibshik55@gmail.com>
2023-10-12 15:16:47 -0700

    f1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print number of arguments passed into it
+ * @argc: the number
+ * @argv: pointer to an array
+ * Return: 0 (success)
+*/
+
+int main(int argc, char *argv[], __attribute__((unused)))
+{
+	printf("%d\n", argc - 1)
+	return (0);
+}
