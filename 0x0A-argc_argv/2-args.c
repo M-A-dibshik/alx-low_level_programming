41dda13dc832bde9b1a053bce42b0c9fcf59651f
Mohammed Abdalla <dibshik55@gmail.com>
2023-10-12 15:51:10 -0700

    f2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints all arguments it receives
+ * main - prints the number of arguments passed into the program
  * @argc: int
  * @argv: list
- * Retrun: 0
+ * Return: 0
 */
 
 int main(int argc, char const *argv[])
 {
-	int j = 0;
+	int x = 0;
 
 	while (argc--)
 	{
-		printf("%s\n", argv[i]);
-		i++;
+		printf("%s\n", argv[x]);
+		x++;
 	}
 	return (0);
 }
