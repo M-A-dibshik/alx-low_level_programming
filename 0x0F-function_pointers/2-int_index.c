e3ec25c218238e1e34a7094ea80175c945e3a720
Mohammed Abdalla <dibshik55@gmail.com>
2023-10-13 06:20:38 -0700

    f2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,10 +1,11 @@
 #include "function_pointers.h"
 
 /**
- * int_index - searches for an integer
+ * int_index - function that searches for integer
  * @array: the int array
  * @size: the array size
  * @cmp: the compare function
+ * Return: the integer index
 */
 
 int int_index(int *array, int size, int (*cmp)(int))
