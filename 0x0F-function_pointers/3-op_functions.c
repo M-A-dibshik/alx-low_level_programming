912e4afa682270dae9c34bef288adbea4fd1046c
Mohammed Abdalla <dibshik55@gmail.com>
2023-10-14 07:10:58 -0700

    f*

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -23,6 +23,29 @@ int op_sub(int a, int b)
 }
 
 /**
+ * op_mul - multiplies two integers
+ * @a: the first int
+ * @b: the second int
+ * Return: the product
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: the first int
+ * @b: the second int
+ * Return: the quotient
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
  * op_mod - modulo two integers
  * @a: the first int
  * @b: the second int
