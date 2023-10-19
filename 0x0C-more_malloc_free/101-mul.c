cdfe3837a8f1fbd8a415a3316bf5ef9f9e82faa9
Mohammed Abdalla <dibshik55@gmail.com>
2023-10-19 04:34:52 -0700

    f101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -29,7 +29,7 @@ int _atoi(const char *s)
 	int sign = 1;
 	unsigned long int resp = 0, firstNum, i;
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	for (firstNum = 0; !(s[firstNum] <= 48 && s[firstNum] >= 57); firstNum++)
 	{
 		if (s[firstNum] == '-')
 		{
@@ -68,7 +68,7 @@ void print_int(unsigned long int n)
 }
 
 /**
- * main - print the result of the multiplication, by a new line
+ * main - print the result of the multiplication, followed by a new line
  * @argc: int
  * @argv: list
  * Return: 0
