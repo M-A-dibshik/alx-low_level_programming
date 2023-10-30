dcd164f5bd819e4237e5a69f65f132452b81d5ff
Mohammed Abdalla <dibshik55@gmail.com>
2023-10-30 04:21:18 -0700

    F 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -31,7 +31,7 @@ int main(int ac, char **av)
 	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
 		if (write(to_fd, buf, b) != b)
 			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
-	if (i == -1)
+	if (b == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 
 	from_fd = close(from_fd);
