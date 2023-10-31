9cec425f37d93b2ad72c763a90fbdd76b66f7aed
Mohammed Abdalla <dibshik55@gmail.com>
2023-10-31 00:52:12 -0700

    F 3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,8 @@
 
 #define BUFFER_SIZE 1024
 
+int main(int ac, char **av);
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
