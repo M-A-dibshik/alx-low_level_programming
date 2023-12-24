a98df7ada69777fa4dc67b6102304cad32023e06
M-A-dibshik <dibshik55@gamil.com>
2023-12-24 09:20:23 +0200

    f 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table to print
+ * Return: void
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *elm;
+	char flag = 0; /* 0 while no data has been printed yet */
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	for (i == 0; i < ht->size; i++)
+	{
+		elm = ht->array[i];
+		while (elm != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", elm->key, elm->value);
+			flag = 1;
+			elm = elm->next;
+		}
+	}
+	printf("}\n");
+}
